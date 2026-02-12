c514a45f75a3ad88075f0ee634cbaffed0f85b2a
Kush Kumar Singh <amiourkush41@gmail.com>
2026-02-13 01:07:27 +0530

    Worked

--- src/components/RestMenu.js
+++ src/components/RestMenu.js
@@ -1,35 +1,36 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router"
+
 import Menu from "./Menu";
+import { restmenu } from "../../texter2";
 
 export default function RestMenu(){
-    const {id} = useParams();
+    //const {id} = useParams();
     const [Restdata,setRestdata] = useState([]);
     useEffect(()=>{
         
-         async function fetchdata() {
-           const proxy="https://cors-anywhere.herokuapp.com/";
-            const swiggy=`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId=${id}`;
-            const response = await fetch(proxy+swiggy);
-            const data= await response.json();
-            const tempdata=data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
-            const filterdata = tempdata.filter((item)=>"title" in item?.card?.card);
-            setRestdata(filterdata);
-           
+         function fetchdata() {
+          
             
+            const tempdata=restmenu?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
+            const filterdata = tempdata.filter((item)=>"title" in item?.card?.card);
+             setRestdata(filterdata);
+           
+           console.log(Restdata);
          }
          fetchdata();
+         
+         
 
 
     },[])
-    return(
-        <>
-        <div className="w-[80vw] mx-auto">
-          {
-            Restdata.map((menu)=><Menu key={Restdata?.card?.card?.title} menu={menu}></Menu>)
-          }
-        </div>
+    // return(
+    //     <>
+    //     <div className="w-[80vw] mx-auto">
+    //       {
+    //         Restdata.map((menu)=><Menu key={menu?.card?.card?.title} menu={menu}></Menu>)
+    //       }
+    //     </div>
         
-        </>
-    )
+    //     </>
+    // )
 }
