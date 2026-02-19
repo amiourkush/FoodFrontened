ff6afd8f472260d534621ecb1e9851b05a3446a0
Kush Kumar Singh <amiourkush41@gmail.com>
2026-02-20 02:58:07 +0530

    Worked on bug

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,10 +1,17 @@
+import { useState } from "react"
 import MenuInfo from "./MenuInfo"
 
 export default function Menu({menu}){
-    return(
+    const [IsOpen,setIsOpen]=useState(true);
+    if(IsOpen){
+
+         return(
         <>
           <div className="w-full">
-            <h1>{menu?.card?.card?.title}</h1>
+            <div className="flex justify-between">
+            <h1 className="text-xl font-extrabold text-red-600">{menu?.card?.card?.title}</h1>
+            <button onClick={()=>{setIsOpen(false)}}>{IsOpen?"up":"down"}</button>
+            </div>
             <div>
                 {
                     menu?.card?.card?.itemCards?.map((menuinfo)=><MenuInfo key={menuinfo?.card?.info?.id} menuinfo={menuinfo}></MenuInfo>)
@@ -13,4 +20,17 @@ export default function Menu({menu}){
           </div>
         </>
     )
+    }
+    else{
+        return(<>
+        <div className="w-full">
+            <div className="flex justify-between">
+            <h1 className="text-xl font-extrabold text-red-600">{menu?.card?.card?.title}</h1>
+            <button onClick={()=>{setIsOpen(true)}}>{IsOpen?"up":"down"}</button>
+            </div>
+            </div>
+        
+        </>)
+    }
+   
 }
--- src/components/MenuInfo.js
+++ src/components/MenuInfo.js
@@ -1,18 +1,20 @@
 export default function MenuInfo({menuinfo}){
     return(
         <>
-        <div className="flex justify-between">
+        <div className="flex justify-between mt-2">
             <div className="w-[80%]">
-                <h2>{menuinfo?.card?.info?.name}</h2>
-                <p>{"Rs"+menuinfo?.card?.info?.defaultPrice/100}</p>
+                <h2 className="text-lg text-black font-bold">{menuinfo?.card?.info?.name}</h2>
+                {/* <p>{"Rs"+menuinfo?.card?.info?.defaultPrice/100}</p> */}
+                <p>{"Rs"+(menuinfo.card.info.defaultPrice?menuinfo?.card?.info?.defaultPrice/100:menuinfo?.card?.info?.price/100)}</p>
                 <p>{menuinfo?.card?.info?.ratings?.aggregatedRating?.rating +"("+menuinfo?.card?.info?.ratings?.aggregatedRating?.ratingCountV2+")"}</p>
                 <p>{menuinfo?.card?.info?.description}</p>
             </div>
             <div className="w-[20%] relative">
-                <img src={"https://media-assets.swiggy.com/swiggy/image/upload/"+menuinfo?.card?.info?.imageId}></img>
-                <button className="absolute bottom-1 px-5 py-2 right-[50%] bg-green-500 rounded-xl font-bold">ADD</button>
+                <img className="object-cover"  src={"https://media-assets.swiggy.com/swiggy/image/upload/"+menuinfo?.card?.info?.imageId}></img>
+                <button className="absolute bottom-1 px-5 py-2 left-18 bg-green-600 rounded-2xl font-bold text-xl text-amber-50 hover:scale-110 hover:shadow-lg cursor-pointer transition-transform duration-300 ease-in-out hover:bg-green-800">ADD</button>
             </div>
         </div>
+        <hr className="mt-2"></hr>
         
         </>
     )
